6f3ee1b75cf443c72ce2bb008f0b10d341341f8e
colin3dmax <colin3dmax@163.com>
2015-04-07 07:22:18 +0800

    调整Chap4_4.3代码结构分离

--- Code/Chap4_4.3/Chap4_4.3/main.cpp
+++ Code/Chap4_4.3/Chap4_4.3/main.cpp
@@ -12,36 +12,9 @@
 #include <iomanip>
 #include "median.h"
 #include "Student_info.h"
+#include "grade.h"
 
 using namespace std;
-
-
-
-typedef vector<double>::size_type vec_sz;
-
-
-
-double grade(double midterm,double final,double homework)
-{
-    return 0.2*midterm + 0.4*final + 0.4 *homework;
-}
-
-
-double grade(double midterm,double final,const vector<double>& hw)
-{
-    if(hw.size()==0){
-        throw domain_error("student has done no homework");
-    }
-    return grade(midterm, final, median(hw) );
-}
-
-
-double grade(const Student_info& s)
-{
-    return grade(s.midterm, s.final, s.homework);
-}
-
-
 
 int main(int argc, const char * argv[]) {
     vector<Student_info> students;
