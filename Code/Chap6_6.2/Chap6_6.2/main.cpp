2b073c78811cafd237bc695ab98f614e098626fa
colin3dmax <colin@qidea.cc>
2015-04-18 09:44:14 +0800

    add Chap6

---
+++ Code/Chap6_6.2/Chap6_6.2/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Chap6_6.2
+//
+//  Created by colin3dmax on 15/4/17.
+//  Copyright (c) 2015年 colin3dmax. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
