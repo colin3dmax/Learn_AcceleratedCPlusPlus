07129e733abc9a1350e019c594fb47df9ec6a7aa
colin3dmax <colin@qidea.cc>
2015-05-05 10:17:23 +0800

    add Chap8

---
+++ Code/Chap8_8.1/Chap8_8.1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chap8_8.1
+//
+//  Created by colin3dmax on 15/5/4.
+//  Copyright (c) 2015年 colin3dmax. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Code/Chap8_8.2.2/Chap8_8.2.2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chap8_8.2.2
+//
+//  Created by colin3dmax on 15/5/5.
+//  Copyright (c) 2015年 colin3dmax. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
